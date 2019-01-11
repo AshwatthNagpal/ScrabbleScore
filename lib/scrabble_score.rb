c7cf3ba7cdf09430410d021aeba772f904407d69
Ashwatth Nagpal <ashwatthnagpal21@gmail.com>
2019-01-11 09:43:39 +0530

    add test case to check whitespaces

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,3 +1,10 @@
-def score(str)
-   0
+def score(str="")
+  if str==""
+    return 0
+  end
+  str.each_char do |i|
+    if i=="\n"||i=="\t"
+      return 0
+    end
+  end
 end
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -1,7 +1,11 @@
 require "scrabble_score.rb"
 
 describe "score" do
-  to "score zero for empty string" do
+  it "score zero for empty string" do
     expect(score("")).to eq 0
   end
+  it "score whitespaces as zero" do
+    expect(score("\n\t")).to eq 0
+  end
+
 end
